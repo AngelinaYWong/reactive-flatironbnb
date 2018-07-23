0fb18af66498c40073998edfb67d546674aa7b61
prmoreira23 <prm.gredes@gmail.com>
2018-07-23 11:07:01 -0400

    Turned Rental into a functional component

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,16 +1,9 @@
 import React from 'react'
 
-class Rental extends React.Component {
-
-  clickHandler = (e) => {
-    this.props.clickedRental(this.props.rentalObj)
-  }
-
-  render() {
-    return (<div className="col-sm-4" onClick={this.clickHandler}><h5>{this.props.rentalObj.name}</h5>
-      <img src={this.props.rentalObj.imageUrl}  className="listingImage img-thumbnail"/>
+  const Rental = (props) => {
+    return (<div className="col-sm-4" onClick={() => props.clickedRental(props.rentalObj)}><h5>{props.rentalObj.name}</h5>
+      <img src={props.rentalObj.imageUrl}  className="listingImage img-thumbnail"/>
       </div>)
   }
-}
 
 export default Rental
